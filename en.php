9803285e9e8f0b8d91b392668e2a87320ce309cf
https://tooly.win <121991856+toolywin@users.noreply.github.com>
2023-01-05 09:43:41 +0700

    Add files via upload

---
+++ en.php
@@ -0,0 +1,314 @@
+<?php
+if( !defined('PSZAPP_INIT') ) exit;
+
+/*
+*	Language	: English
+*	Code		: EN
+*	Translator	: Phong Thai
+*	Contact		: https://tooly.win - prescriptz.com@gmail.com
+*/
+
+/*
+** Instructions to translate this file to your language
+
+ - Please DO NOT translate:
+ 	+ $lang indexes
+ 	+ Text links
+ 	+ Language codes
+ 	+ PHP variables, PHP function parameters
+ 	+ Uppercase strings in braces
+ - Please KEEP:
+ 	+ formats of UPPERCASE / lowercase
+ 	+ the order of arrays
+
+ - Samples:
+/////
+$lang['do not translate this part'] = 'Please translate this part to your language';
+$lang['index - do not translate']   = 'Translate but leave <a href="/do-not-translate/">translate here, too</a>, keep translating';
+$lang['index - do not']             = 'Translate outside {{DO-NOT-TRANSLATE}} and translate this too';
+
+$lang['index - do not']             = array('Translate this part 1', 'Translate this part 2', 'Translate this part 3');
+$lang['index - do not']             = array('Translate this index'	=> 'Translate this value, too');
+/////
+*/
+
+$lang = [];
+
+$lang['just now'] = 'just now';
+$lang['~ 15 mins ago'] = '~ 15 mins ago';
+$lang['less than 15 minutes ago'] = 'less than 15 minutes ago';
+$lang['~ 30 mins ago'] = '~ 30 mins ago';
+$lang['less than 30 minutes ago'] = 'less than 30 minutes ago';
+$lang['~ 1 hr ago'] = '~ 1 hr ago';
+$lang['less than 1 hour ago'] = 'less than 1 hour ago';
+$lang['~ %d hrs ago'] = '~ %d hrs ago';
+$lang['about %d hours ago'] = 'about %d hours ago';
+$lang['~ %d days ago'] = '~ %d days ago';
+$lang['about %d days ago'] = 'about %d days ago';
+$lang['< %d weeks ago'] = '< %d weeks ago';
+$lang['less than %d weeks ago'] = 'less than %d weeks ago';
+$lang['< 1 mon ago'] = '< 1 mon ago';
+$lang['less than 1 month ago'] = 'less than 1 month ago';
+$lang['< %d mons ago'] = '< %d mons ago';
+$lang['less than %d months ago'] = 'less than %d months ago';
+$lang['Monday'] = 'Monday';
+$lang['Mon'] = 'Mon';
+$lang['Tuesday'] = 'Tuesday';
+$lang['Tue'] = 'Tue';
+$lang['Wednesday'] = 'Wednesday';
+$lang['Wed'] = 'Wed';
+$lang['Thursday'] = 'Thursday';
+$lang['Thu'] = 'Thu';
+$lang['Friday'] = 'Friday';
+$lang['Fri'] = 'Fri';
+$lang['Saturday'] = 'Saturday';
+$lang['Sat'] = 'Sat';
+$lang['Sunday'] = 'Sunday';
+$lang['Sun'] = 'Sun';
+$lang['January'] = 'January';
+$lang['February'] = 'February';
+$lang['March'] = 'March';
+$lang['April'] = 'April';
+$lang['May'] = 'May';
+$lang['June'] = 'June';
+$lang['July'] = 'July';
+$lang['August'] = 'August';
+$lang['September'] = 'September';
+$lang['October'] = 'October';
+$lang['November'] = 'November';
+$lang['December'] = 'December';
+$lang['Jan'] = 'Jan';
+$lang['Feb'] = 'Feb';
+$lang['Mar'] = 'Mar';
+$lang['Apr'] = 'Apr';
+$lang['May'] = 'May';
+$lang['Jun'] = 'Jun';
+$lang['Jul'] = 'Jul';
+$lang['Aug'] = 'Aug';
+$lang['Sep'] = 'Sep';
+$lang['Oct'] = 'Oct';
+$lang['Nov'] = 'Nov';
+$lang['Dec'] = 'Dec';
+
+$lang['years'] = 'years';
+$lang['year'] = 'year';
+$lang['months'] = 'months';
+$lang['month'] = 'month';
+$lang['days'] = 'days';
+$lang['day'] = 'day';
+$lang['hours'] = 'hours';
+$lang['hour'] = 'hour';
+$lang['Changelog'] = 'Changelog';
+$lang['Contact'] = 'Contact';
+$lang['Close'] = 'Close';
+$lang['Yes'] = 'Yes';
+$lang['No'] = 'No';
+$lang['Register'] = 'Register';
+$lang['Login'] = 'Login';
+$lang['Submit'] = 'Submit';
+$lang['Download'] = 'Download';
+$lang['optional'] = 'optional';
+$lang['required'] = 'required';
+$lang['Required'] = 'Required';
+$lang['Name'] = 'Name';
+$lang['Description'] = 'Description';
+$lang['Keyword'] = 'Keyword';
+$lang['CAUTION'] = 'CAUTION';
+$lang['of'] = 'of';
+$lang['Sample'] = 'Sample';
+$lang['Clear'] = 'Clear';
+$lang['Load'] = 'Load';
+$lang['Caution'] = 'Caution';
+$lang['loading'] = 'loading';
+$lang['Submit'] = 'Submit';
+$lang['Reset'] = 'Reset';
+$lang['Invalid'] = 'Invalid';
+$lang['encoded'] = 'encoded';
+$lang['to'] = 'to';
+$lang['decoded'] = 'decoded';
+$lang['Share on'] = 'Share on';
+$lang['text'] = 'text';
+$lang['message'] = 'message';
+$lang['picture'] = 'picture';
+$lang['link'] = 'link';
+$lang['video'] = 'video';
+$lang['audio'] = 'audio';
+$lang['email'] = 'email';
+$lang['text file'] = 'text file';
+$lang['one'] = 'one';
+$lang['Encode'] = 'Encode';
+$lang['Decode'] = 'Decode';
+$lang['Total'] = 'Total';
+$lang['Contact'] = 'Contact';
+$lang['Donate'] = 'Donate';
+$lang['Developer'] = 'Developer';
+$lang['Version'] = 'Version';
+$lang['View'] = 'View';
+$lang['Verified'] = 'Verified';
+$lang['Fixed'] = 'Fixed';
+$lang['then'] = 'then';
+$lang['Install'] = 'Install';
+
+$lang['Added'] = 'Added';
+$lang['Suggest'] = 'Suggest';
+$lang['Solid'] = 'Solid';
+$lang['Regular'] = 'Regular';
+$lang['Light'] = 'Light';
+$lang['Thin'] = 'Thin';
+$lang['Duotone'] = 'Duotone';
+
+$lang['Not Now'] = 'Not Now';
+$lang['Just tap'] = 'Just tap';
+$lang['Source Code'] = 'Source Code';
+$lang['Please wait'] = 'Please wait';
+$lang['Share via email'] = 'Share via email';
+$lang['Import from the external URL'] = 'Import from the external URL';
+$lang['Import from the local file'] = 'Import from the local file';
+$lang['Copy to clipboard'] = 'Copy to clipboard';
+$lang['Click to open'] = 'Click to open';
+$lang['Add to favarite'] = 'Add to favarite';
+$lang['Copy Link'] = 'Copy Link';
+$lang['Choose a language'] = 'Choose a language';
+$lang['Search a language'] = 'Search a language';
+
+$lang['Initial v1.0.0 released'] = 'Initial v1.0.0 released';
+$lang['idea started'] = 'idea started';
+$lang['Tool Developer Guidelines'] = 'Tool Developer Guidelines';
+$lang['Everyone may develop & submit their tools, once approved, the tools are live for all to use for free'] = 'Everyone may develop & submit their tools, once approved, the tools are live for all to use for free';
+$lang['Open Tool Hub by Any One for All'] = 'Open Tool Hub by Any One for All';
+$lang['Open tool hub for free to use by any one for every one with hundreds of tools for encoding, decoding, converting, filtering, replacing, generating strings, and much more that you could imagine.'] = 'Open tool hub for free to use by any one for every one with hundreds of tools for encoding, decoding, converting, filtering, replacing, generating strings, and much more that you could imagine.';
+$lang['Homepage keywords'] = 'code beautify, code formatter, code minify, data convert, code parse, data parser, string encoder, text decoder, image editor';
+$lang['Copied to clipboard'] = 'Copied to clipboard';
+$lang['Add to favorite'] = 'Add to favorite';
+$lang['Remove from favorite'] = 'Remove from favorite';
+$lang['Enter your input to process'] = 'Enter your input to process';
+$lang['Do you want to log out?'] = 'Do you want to log out?';
+$lang['Logging out'] = 'Logging out';
+$lang['Logged out. Please reload the page to see changes'] = 'Logged out. Please reload the page to see changes';
+$lang['Download from'] = 'Download from';
+$lang['Icon Switcher'] = 'Icon Switcher';
+$lang['Theme Switcher'] = 'Theme Switcher';
+$lang['Language Switcher'] = 'Language Switcher';
+$lang['Favorite list'] = 'Favorite list';
+$lang['My Profile'] = 'My Profile';
+$lang['Sign Out'] = 'Sign Out';
+$lang['How it Works'] = 'How it Works';
+$lang['Register an account'] = 'Register an account';
+$lang['Feel free to register an account without any buck'] = 'Feel free to register an account without any buck';
+$lang['Develop your tool'] = 'Develop your tool';
+$lang['Follow our instructions to develop your own tools'] = 'Follow our instructions to develop your own tools';
+$lang['Once your submitted tool approved, it would be live for all to use'] = 'Once your submitted tool approved, it would be live for all to use';
+$lang['Knowledge Base'] = 'Knowledge Base';
+$lang['From this section, we will try to help you understand & solve any issue relates to the tool development'] = 'From this section, we will try to help you understand & solve any issue relates to the tool development';
+$lang['Contact Us'] = 'Contact Us';
+$lang['What is a tool?'] = 'What is a tool?';
+$lang['A tool is just a set of files that used to serve your tool, you could use any framework, any technology to build your tool'] = 'A tool is just a set of files that used to serve your tool, you could use any framework, any technology to build your tool';
+$lang['Structure of a tool package'] = 'Structure of a tool package';
+$lang['First rule for a tool package is all files & folders must be at root of zipped file, not at any sub-level'] = 'First rule for a tool package is all files & folders must be at root of zipped file, not at any sub-level';
+$lang['Second, you may have unlimited files and folder in a tool package, but'] = 'Second, you may have unlimited files and folder in a tool package, but';
+$lang['are required files. These only files shall be loaded whenenver tool accessed. You will process and handle all things in these files to serve the visitors'] = 'are required files. These only files shall be loaded whenenver tool accessed. You will process and handle all things in these files to serve the visitors';
+$lang['Download our sample built tool and see how it works'] = 'Download our sample built tool and see how it works';
+$lang['Required if your tool provides the public API services'] = 'Required if your tool provides the public API services';
+$lang['Start to process everything in your tool'] = 'Start to process everything in your tool';
+$lang['Contains the global settings of your tool'] = 'Contains the global settings of your tool';
+$lang['Used to sharing on the socials'] = 'Used to sharing on the socials';
+$lang['Used to display on the title bars of browsers'] = 'Used to display on the title bars of browsers';
+$lang['The tool logo'] = 'The tool logo';
+$lang['Contains the PHP language files, the language codes are same with Google Translate'] = 'Contains the PHP language files, the language codes are same with Google Translate';
+$lang['We may help you translate them, you only need to provide the standard English file'] = 'We may help you translate them, you only need to provide the standard English file';
+$lang['You may consider a tool like a mini PHP script, you may also use any material, any resource and any framework, such as Lavarel or Zend, to build your tools. Just use like you want and build as you wish'] = 'You may consider a tool like a mini PHP script, you may also use any material, any resource and any framework, such as Lavarel or Zend, to build your tools. Just use like you want and build as you wish';
+$lang['But we also recommend you to use our built-in resources to build faster, our resources almost coded line by line for the system, so it gives the best performance & speed for the website'] = 'But we also recommend you to use our built-in resources to build faster, our resources almost coded line by line for the system, so it gives the best performance & speed for the website';
+$lang['Unchangable variable name'] = 'Unchangable variable name';
+$lang['latest version'] = 'latest version';
+$lang['name of tool'] = 'name of tool';
+$lang['tool description, used in meta tag'] = 'tool description, used in meta tag';
+$lang['tool keywords, used in meta tag'] = 'tool keywords, used in meta tag';
+$lang['name of developer'] = 'name of developer';
+$lang['email or website'] = 'email or website';
+$lang['links to donations'] = 'links to donations';
+$lang['created date; format'] = 'created date; format';
+$lang['used to store changelog'] = 'used to store changelog';
+$lang['Usable global variables and functions'] = 'Usable global variables and functions';
+$lang['There are few variables and functions should be used to create a tool'] = 'There are few variables and functions should be used to create a tool';
+$lang['we use'] = 'we use';
+$lang['for templating all static HTML files. But you may also use plain PHP code to retrieve these variable values by alternative functions'] = 'for templating all static HTML files. But you may also use plain PHP code to retrieve these variable values by alternative functions';
+$lang['current language'] = 'current language';
+$lang['current language code'] = 'current language code';
+$lang['app name'] = 'app name';
+$lang['site name'] = 'site name';
+$lang['app site domain or sub-domain, without http://www.'] = 'app site domain or sub-domain, without http://www.';
+$lang['email of super administrative account'] = 'email of super administrative account';
+$lang['path to root of website, normally it is root of domain'] = 'path to root of website, normally it is root of domain';
+$lang['path to root of website app'] = 'path to root of website app';
+$lang['path to root of used current frontpage'] = 'path to root of used current frontpage';
+$lang['title of current page, used for search engines'] = 'title of current page, used for search engines';
+$lang['short description of current page, used for search engines'] = 'short description of current page, used for search engines';
+$lang['keywords of current page, used for search engines'] = 'keywords of current page, used for search engines';
+$lang['author name of current page, used for search engines'] = 'author name of current page, used for search engines';
+$lang['author email of current page, used for search engines'] = 'author email of current page, used for search engines';
+$lang['full sharing image url of current page, used for search engines'] = 'full sharing image url of current page, used for search engines';
+$lang['full sharing url of current page, used for sharing'] = 'full sharing url of current page, used for sharing';
+$lang['below global variables are available for the tool page only'] = 'below global variables are available for the tool page only';
+$lang['tool'] = 'tool';
+$lang['tool slug'] = 'tool slug';
+$lang['tool name'] = 'tool name';
+$lang['tool description'] = 'tool description';
+$lang['tool developer name'] = 'tool developer name';
+$lang['tool developer link'] = 'tool developer link';
+$lang['Serving Time'] = 'Serving Time';
+$lang['total count of tool usage'] = 'total count of tool usage';
+$lang['total count of tool usage in today'] = 'total count of tool usage in today';
+$lang['total count of tool usage in current week'] = 'total count of tool usage in current week';
+$lang['total count of tool usage in current month'] = 'total count of tool usage in current month';
+$lang['total count of tool API usage only'] = 'total count of tool API usage only';
+$lang['Currently there is no tool available'] = 'Currently there is no tool available';
+$lang['Sorry but this tool is not available at this moment'] = 'Sorry but this tool is not available at this moment';
+$lang['Perhaps this tool is under maintaince, please retry later'] = 'Perhaps this tool is under maintaince, please retry later';
+$lang['Import from the external URL'] = 'Import from the external URL';
+$lang['Enter the full URL to load'] = 'Enter the full URL to load';
+$lang['We are using the external service from'] = 'We are using the external service from';
+$lang['to load your external URL directly, no any data stored at both our side and your client'] = 'to load your external URL directly, no any data stored at both our side and your client';
+$lang['There is no tool with your query'] = 'There is no tool with your query';
+$lang['Quick access via'] = 'Quick access via';
+$lang['Share this tool'] = 'Share this tool';
+$lang['Logged-in members may choose to hide their activities'] = 'Logged-in members may choose to hide their activities';
+$lang['Browser Shortcut'] = 'Browser Shortcut';
+$lang['Drag-n-drop this below shortcut into your browser bookmark bar to use this tool in one click'] = 'Drag-n-drop this below shortcut into your browser bookmark bar to use this tool in one click';
+$lang['Select the text'] = 'Select the text';
+$lang['Click your shortcut to encode / decode'] = 'Click your shortcut to encode / decode';
+$lang['Latest tasks'] = 'Latest tasks';
+$lang['The tool could not process this input directly, please access the website to re-enter this input to process again'] = 'The tool could not process this input directly, please access the website to re-enter this input to process again';
+$lang['Please enter the decoded text to convert into normal string'] = 'Please enter the decoded text to convert into normal string';
+$lang['Change the language to get the bookmark in your language'] = 'Change the language to get the bookmark in your language';
+$lang['Date Added'] = 'Date Added';
+$lang['Description'] = 'Description';
+$lang['Direct Usage'] = 'Direct Usage';
+$lang['This tool currently accepts the method POST via API only, if you would like to use the method GET, please consider using'] = 'This tool currently accepts the method POST via API only, if you would like to use the method GET, please consider using';
+$lang['Endpoint'] = 'Endpoint';
+$lang['Parameters'] = 'Parameters';
+$lang['Respone'] = 'Respone';
+$lang['if your request is okay'] = 'if your request is okay';
+$lang['the result of your request if there is no error'] = 'the result of your request if there is no error';
+$lang['the message error if there is any error'] = 'the message error if there is any error';
+$lang['Result'] = 'Result';
+$lang['Usage Count'] = 'Usage Count';
+$lang['Today'] = 'Today';
+$lang['This week'] = 'This week';
+$lang['This month'] = 'This month';
+$lang['API Calls'] = 'API Calls';
+$lang['character'] = 'character';
+$lang['characters'] = 'characters';
+$lang['Invalid URL'] = 'Invalid URL';
+$lang['Error in tranmissing the data to'] = 'Error in tranmissing the data to';
+$lang['please retry'] = 'please retry';
+$lang['FAQ'] = 'FAQ';
+$lang['This tool currently does not support API'] = 'This tool currently does not support API';
+
+// v1.1.0
+$lang['Links to open source sites if tool is available on'] = 'Links to open source sites if tool is available on';
+$lang['Load content of tool changelog via AJAX'] = 'Load content of tool changelog via AJAX';
+$lang['Text file to store the main version'] = 'Text file to store the main version';
+$lang['Link to login page'] = 'Link to login page';
+$lang['Install the web app'] = 'Install the web app';
+$lang['on your home screen for quicker and easier access'] = 'on your home screen for quicker and easier access';
+$lang['Add to Home Screen'] = 'Add to Home Screen';
